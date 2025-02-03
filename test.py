7495048bd94b222713b653849d066c21c541daa8
takhahashi <yuu1719m@ezweb.ne.jp>
2025-02-03 17:30:54 +0900

    a

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
 
     @hydra.main(
-        config_name='configs/test.yaml',
+        config_name='configs/test',
         validate=True,
     )
     def main(config):
