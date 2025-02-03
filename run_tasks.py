c68e87947cf76b63bc862097929116e5651c907c
takhahashi <yuu1719m@ezweb.ne.jp>
2025-02-03 16:26:08 +0900

    add file

---
+++ run_tasks.py
@@ -0,0 +1,21 @@
+import os
+import hydra
+import logging
+import pdb
+
+log = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+
+    @hydra.main(
+        config_path=os.path.dirname(os.environ["HYDRA_CONFIG_PATH"]),
+        config_name=os.path.basename(os.environ["HYDRA_CONFIG_PATH"]),
+    )
+    def main(config):
+        auto_generated_dir = os.getcwd()
+        log.info(f"Work dir: {auto_generated_dir}")
+        os.chdir(hydra.utils.get_original_cwd())
+
+        #run_glue_for_model_series_fast(config, auto_generated_dir)
+
+    main()
