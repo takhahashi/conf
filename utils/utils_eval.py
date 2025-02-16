62ece12604d2269bf36ab410fd63b0e29682ac49
takhahashi <yuu1719m@ezweb.ne.jp>
2025-02-16 22:43:50 +0900

    s

--- utils/utils_eval.py
+++ utils/utils_eval.py
@@ -28,9 +28,7 @@ def evaluate_model(config, model, datasets):
     reg_output = []
     logits = []
     for step, inputs in enumerate(test_dataloader):
-        for key, value in inputs.items():
-            print(f"  {key} is on device: {value.device}")
-        exit()
+        inputs = {k:v.cuda() for k, v in inputs.itemize}
         outputs = model(**inputs, output_hidden_states=True)
         hidden_states.append(outputs.hidden_states[-1][:, 0, :].to('cpu').detach().numpy().copy())
         if config.model.model_type == "hybrid":
