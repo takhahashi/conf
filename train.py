d00682a149bf0ae9eff09d6d0f7e6d6c3083266b
takhahashi <yuu1719m@ezweb.ne.jp>
2025-02-20 19:19:47 +0900

    s#	modified:   train.py

--- train.py
+++ train.py
@@ -98,7 +98,6 @@ def train_model(config, training_args, data_args, work_dir=None):
 def update_config(cfg_old, cfg_new):
     print(cfg_new['lamb'].type())
     for k, v in cfg_new.items():
-        print(k)
         if k in cfg_old.__dict__:
             print(k)
             setattr(cfg_old, k, v)
