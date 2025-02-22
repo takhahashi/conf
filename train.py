66115bb45716934efe9d03dfc886e56f61df2a7d
takhahashi <yuu1719m@ezweb.ne.jp>
2025-02-22 16:59:59 +0900

    s

--- train.py
+++ train.py
@@ -169,7 +169,7 @@ def train_gp(config, training_args, data_args, work_dir=None):
             GPmodel.likelihood.noise.item()
         ))
     model_savepath = Path(config.training.output_dir) / "gp_model"
-    model_savepath.parent.mkdir(parents=True, exist_ok=True)
+    model_savepath.parent.mkdir(parents=True)
     torch.save(GPmodel.state_dict(), model_savepath)
 
 def update_config(cfg_old, cfg_new):
